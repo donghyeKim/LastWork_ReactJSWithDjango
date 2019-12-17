6c0cd84968aa6f3f0dd2beb180fc46ec45557117
donghyeKim <arora_@naver.com>
2019-12-17 15:19:33 +0900

    커밋

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\amorf\desktop\멋사 스터디\ㄹㅇ마지막_리액트_장고crud\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
